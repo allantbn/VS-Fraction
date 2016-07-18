8c3bb594287b8dbe2f3ea4d84de341c05bf71365
allantbn <allan.tan@titansoft.com.sg>
2016-07-18 15:31:47 +0800

    First test zero plus zerp

---
+++ FractionTest/FractionTest/AddFractionsTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FractionTest
+{
+    [TestClass]
+    public class AddFractionsTest
+    {
+        [TestMethod]
+        public void ZeroPlusZero()
+        {
+            Fraction sum = new Fraction(0).Plus(new Fraction(0));
+            Assert.AreEqual(0, sum.IntValue());
+        }
+    }
+}
---
+++ FractionTest/FractionTest/Fraction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FractionTest
+{
+    public class Fraction
+    {
+        public Fraction (int integerValue) {
+            
+        }
+
+        public Fraction Plus(Fraction fraction)
+        {
+            return this;
+        }
+
+        public int IntValue()
+        {
+            return 0;
+        }
+    }
+}
