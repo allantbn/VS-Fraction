8329ac4b9c74a52fce59c9d81a3f1fdb226cdfdc
allantbn <allan.tan@titansoft.com.sg>
2016-07-19 17:18:53 +0800

    no longer fake numberator

--- FractionTest/FractionTest/Fraction.cs
+++ FractionTest/FractionTest/Fraction.cs
@@ -6,16 +6,19 @@ namespace FractionTest
     {
         private readonly int _integerValue;
         private readonly int _denominator;
+        private readonly int _numerator;
 
         public Fraction (int integerValue)
         {
             _integerValue = integerValue;
+            _numerator = integerValue;
             _denominator = 1;
         }
 
         public Fraction(int numerator, int denominator)
         {
             _integerValue = numerator;
+            _numerator = numerator;
             _denominator = denominator;
         }
 
@@ -31,7 +34,7 @@ namespace FractionTest
 
         internal int GetNumerator()
         {
-            return 3;
+            return _numerator;
         }
 
         internal int GetDenominator()
