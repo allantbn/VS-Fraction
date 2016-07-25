99f06b16b7780acd6564d1453e88fa283db85f3f
allantbn <allan.tan@titansoft.com.sg>
2016-07-25 15:01:41 +0800

    confident enough that equals() works, except for reducing

--- FractionTest/FractionTest/FractionEqualsTest.cs
+++ FractionTest/FractionTest/FractionEqualsTest.cs
@@ -11,5 +11,29 @@ namespace FractionTest
         {
             Assert.AreEqual(new Fraction(3, 5), new Fraction(3,5));
         }
+
+        [TestMethod]
+        public void DifferentNumerators()
+        {
+            Assert.AreNotEqual(new Fraction(1, 5), new Fraction(2, 5));
+        }
+
+        [TestMethod]
+        public void DIfferentDenominators()
+        {
+            Assert.AreNotEqual(new Fraction(3, 4), new Fraction(3, 7));
+        }
+
+        [TestMethod]
+        public void WholeNumberEqualsSameFraction()
+        {
+            Assert.AreEqual(new Fraction(5, 1), new Fraction(5));
+        }
+
+        [TestMethod]
+        public void WholeNumberNotEqualToDifferentWholeNumber()
+        {
+            Assert.AreNotEqual(new Fraction(5), new Fraction(6));
+        }
     }
 }
